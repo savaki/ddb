f44124ccfc56d0dee39b334a3854ad49acf18e10
savaki <matt.ho@gmail.com>
2019-03-11 09:42:15 -0700

    externalized expression handling into expression type

---
+++ expression.go
@@ -0,0 +1,181 @@
+package ddb
+
+import (
+	"bytes"
+	"io"
+	"regexp"
+	"strconv"
+	"strings"
+	"sync/atomic"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+)
+
+var (
+	reKeys   = regexp.MustCompile(`(#[a-zA-Z][a-zA-Z0-9._]*)`)
+	reValues = regexp.MustCompile(`\?`)
+)
+
+type expression struct {
+	attributes []*attributeSpec
+	names      map[string]*string
+	values     map[string]*dynamodb.AttributeValue
+	index      int64
+
+	adds    *bytes.Buffer
+	deletes *bytes.Buffer
+	removes *bytes.Buffer
+	sets    *bytes.Buffer
+}
+
+func (e *expression) setExpressionAttributeName(name string) (string, error) {
+	if e.names == nil {
+		e.names = map[string]*string{}
+	}
+
+	for _, attr := range e.attributes {
+		switch name[1:] {
+		case attr.AttributeName:
+			name = "#" + attr.FieldName
+		case attr.FieldName:
+			// ok
+		default:
+			continue
+		}
+
+		e.names[name] = aws.String(attr.AttributeName)
+		return name, nil
+	}
+
+	return "", errorf(ErrInvalidFieldName, "invalid field name, %v", name)
+}
+
+func (e *expression) setExpressionAttributeValue(value Value) string {
+	if e.values == nil {
+		e.values = map[string]*dynamodb.AttributeValue{}
+	}
+
+	id := atomic.AddInt64(&e.index, 1)
+	name := ":field" + strconv.FormatInt(id, 10)
+	e.values[name] = value.item
+
+	return name
+}
+
+func (e *expression) String() string {
+	size := e.Size()
+	buf := bytes.NewBuffer(make([]byte, 0, size))
+
+	//if e.adds != nil {
+	//	buf.Write(e.adds.Bytes())
+	//	io.WriteString(buf, " ")
+	//}
+	//if e.deletes != nil {
+	//	buf.Write(e.deletes.Bytes())
+	//	io.WriteString(buf, " ")
+	//}
+	//if e.removes != nil {
+	//	buf.Write(e.removes.Bytes())
+	//	io.WriteString(buf, " ")
+	//}
+	if e.sets != nil {
+		buf.Write(e.sets.Bytes())
+		io.WriteString(buf, " ")
+	}
+
+	// strip off trailing space
+	if buf.Len() > 0 {
+		buf.Truncate(buf.Len() - 1)
+	}
+
+	return buf.String()
+}
+
+func (e *expression) Size() int {
+	size := 0
+	//if e.adds != nil {
+	//	size += e.adds.Len() + 1
+	//}
+	//if e.deletes != nil {
+	//	size += e.deletes.Len() + 1
+	//}
+	//if e.removes != nil {
+	//	size += e.removes.Len() + 1
+	//}
+	if e.sets != nil {
+		size += e.sets.Len() + 1
+	}
+	return size
+}
+
+func (e *expression) append(buf *bytes.Buffer, keyword, expr string, values ...Value) error {
+	// names
+	//
+	matches := reKeys.FindAllStringSubmatch(expr, -1)
+	for _, match := range matches {
+		name := match[1]
+		updatedName, err := e.setExpressionAttributeName(name)
+		if err != nil {
+			return err
+		}
+
+		if name != updatedName {
+			expr = strings.Replace(expr, name, updatedName, -1)
+		}
+	}
+
+	// values
+	//
+	matches = reValues.FindAllStringSubmatch(expr, -1)
+	if len(matches) != len(values) {
+		return errorf(ErrMismatchedValueCount, "Set expression, %v, contains %v values, but received %v values", expr, len(matches), len(values))
+	}
+	for index := range matches {
+		value := values[index]
+		fieldName := e.setExpressionAttributeValue(value)
+		expr = strings.Replace(expr, "?", fieldName, 1)
+	}
+
+	// expr
+	//
+	if buf.Len() == 0 {
+		if len(keyword) > 0 {
+			buf.WriteString(keyword)
+			buf.WriteString(" ")
+		}
+	} else {
+		buf.WriteString(", ")
+	}
+	buf.WriteString(strings.TrimSpace(expr))
+
+	return nil
+}
+
+//func (e *expression) Add(expr string, values ...Value) error {
+//	if e.adds == nil {
+//		e.adds = bytes.NewBuffer(make([]byte, 0, 128))
+//	}
+//	return e.append(e.adds, "Add", expr, values...)
+//}
+//
+//func (e *expression) Delete(expr string, values ...Value) error {
+//	if e.deletes == nil {
+//		e.deletes = bytes.NewBuffer(make([]byte, 0, 128))
+//	}
+//	return e.append(e.deletes, "Delete", expr, values...)
+//}
+//
+//func (e *expression) Remove(expr string, values ...Value) error {
+//	if e.removes == nil {
+//		e.removes = bytes.NewBuffer(make([]byte, 0, 128))
+//	}
+//	return e.append(e.removes, "Remove", expr, values...)
+//}
+
+func (e *expression) Set(expr string, values ...Value) error {
+	if e.sets == nil {
+		e.sets = bytes.NewBuffer(make([]byte, 0, 128))
+	}
+	return e.append(e.sets, "Set", expr, values...)
+}
--- update.go
+++ update.go
@@ -1,21 +1,11 @@
 package ddb
 
 import (
-	"bytes"
 	"context"
-	"regexp"
-	"strconv"
-	"strings"
-	"sync/atomic"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/dynamodb"
 	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbiface"
-)
-
-var (
-	reKeys   = regexp.MustCompile(`(#[a-zA-Z][a-zA-Z0-9._]*)`)
-	reValues = regexp.MustCompile(`\?`)
 )
 
 type Update struct {
@@ -24,37 +14,20 @@ type Update struct {
 	hashKey        Value
 	rangeKey       Value
 	consistentRead bool
-	names          map[string]*string
-	values         map[string]*dynamodb.AttributeValue
 	consumed       *ConsumedCapacity
-	index          int64
 	err            error
-	set            *bytes.Buffer
-}
-
-func (u *Update) makeUpdateExpression() string {
-	n := 0
-	if u.set != nil {
-		n += u.set.Len()
-	}
-
-	buf := bytes.NewBuffer(make([]byte, 0, n))
-	if u.set.Len() > 0 {
-		buf.WriteString(u.set.String())
-	}
-
-	return buf.String()
+	updates        *expression
 }
 
 func (u *Update) makeUpdateItemInput() *dynamodb.UpdateItemInput {
 	var (
 		key              = makeKey(u.spec, u.hashKey, u.rangeKey)
-		updateExpression = u.makeUpdateExpression()
+		updateExpression = u.updates.String()
 	)
 
 	return &dynamodb.UpdateItemInput{
-		ExpressionAttributeNames:  u.names,
-		ExpressionAttributeValues: u.values,
+		ExpressionAttributeNames:  u.updates.names,
+		ExpressionAttributeValues: u.updates.values,
 		Key:                       key,
 		ReturnConsumedCapacity:    aws.String(dynamodb.ReturnConsumedCapacityTotal),
 		TableName:                 aws.String(u.spec.TableName),
@@ -67,76 +40,10 @@ func (u *Update) Range(value Value) *Update {
 	return u
 }
 
-func (u *Update) setExpressionAttributeName(name string) string {
-	if u.names == nil {
-		u.names = map[string]*string{}
-	}
-
-	for _, attr := range u.spec.Attributes {
-		switch name[1:] {
-		case attr.AttributeName:
-			name = "#" + attr.FieldName
-		case attr.FieldName:
-			// ok
-		default:
-			continue
-		}
-
-		u.names[name] = aws.String(attr.AttributeName)
-		return name
-	}
-
-	u.err = errorf(ErrInvalidFieldName, "invalid field name, %v", name)
-	return ""
-}
-
-func (u *Update) setExpressionAttributeValue(value Value) string {
-	if u.values == nil {
-		u.values = map[string]*dynamodb.AttributeValue{}
-	}
-
-	id := atomic.AddInt64(&u.index, 1)
-	name := ":field" + strconv.FormatInt(id, 10)
-	u.values[name] = value.item
-
-	return name
-}
-
 func (u *Update) Set(expr string, values ...Value) *Update {
-	expr = strings.TrimSpace(expr)
-
-	// handle keys
-	matches := reKeys.FindAllStringSubmatch(expr, -1)
-	for _, match := range matches {
-		name := match[1]
-		updatedName := u.setExpressionAttributeName(name)
-		if name != updatedName {
-			expr = strings.Replace(expr, name, updatedName, -1)
-		}
+	if err := u.updates.Set(expr, values...); err != nil {
+		u.err = err
 	}
-
-	// handle values
-	matches = reValues.FindAllStringSubmatch(expr, -1)
-	if len(matches) != len(values) {
-		u.err = errorf(ErrMismatchedValueCount, "Set expression, %v, contains %v values, but received %v values", expr, len(matches), len(values))
-		return u
-	}
-	for index := range matches {
-		value := values[index]
-		fieldName := u.setExpressionAttributeValue(value)
-		expr = strings.Replace(expr, "?", fieldName, 1)
-	}
-
-	if u.set == nil {
-		u.set = bytes.NewBuffer(nil)
-	}
-
-	if u.set.Len() == 0 {
-		u.set.WriteString("Set ")
-	} else {
-		u.set.WriteString(", ")
-	}
-	u.set.WriteString(expr)
 
 	return u
 }
@@ -166,5 +73,8 @@ func (t *Table) Update(hashKey Value) *Update {
 		spec:     t.spec,
 		hashKey:  hashKey,
 		consumed: t.consumed,
+		updates: &expression{
+			attributes: t.spec.Attributes,
+		},
 	}
 }
