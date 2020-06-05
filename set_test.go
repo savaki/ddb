3d358198e0a22595f49bdda55849100844174e3f
Matt Ho <matt.ho@gmail.com>
2020-06-05 01:09:51 -0500

    added test suite for StringSet

---
+++ set_test.go
@@ -0,0 +1,26 @@
+package ddb
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
+)
+
+func TestStringSet(t *testing.T) {
+	want := StringSet{"a", "b", "c"}
+
+	item, err := dynamodbattribute.Marshal(want)
+	if err != nil {
+		t.Fatalf("got %v; want nil", err)
+	}
+
+	var got StringSet
+	err = dynamodbattribute.Unmarshal(item, &got)
+	if err != nil {
+		t.Fatalf("got %v; want nil", err)
+	}
+	if !reflect.DeepEqual(got, want) {
+		t.Fatalf("got %v; want %v", got, want)
+	}
+}
