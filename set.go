e2ea0315db2d762996b1877da9314ce52f8b11c5
Matt Ho <matt.ho@gmail.com>
2020-09-24 16:23:05 -0500

    added support for Int64Set

--- set.go
+++ set.go
@@ -1,29 +1,63 @@
 package ddb
 
 import (
+	"fmt"
+	"strconv"
+
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/dynamodb"
 )
+
+// Int64Set represents an array expressed as a set.
+// (otherwise than a List which would be the default)
+type Int64Set []int64
+
+// MarshalDynamoDBAttributeValue implements Marshaler
+func (ii Int64Set) MarshalDynamoDBAttributeValue(item *dynamodb.AttributeValue) error {
+	for _, i := range ii {
+		item.NS = append(item.NS, aws.String(strconv.FormatInt(i, 10)))
+	}
+	return nil
+}
+
+// UnmarshalDynamoDBAttributeValue implements Unmarshaler
+func (ii *Int64Set) UnmarshalDynamoDBAttributeValue(item *dynamodb.AttributeValue) error {
+	if item == nil || item.NS == nil {
+		return nil
+	}
+
+	var vv []int64
+	for _, ns := range item.NS {
+		v, err := strconv.ParseInt(*ns, 10, 64)
+		if err != nil {
+			return fmt.Errorf("failed to parse int64, %v: %w", *ns, err)
+		}
+		vv = append(vv, v)
+	}
+
+	*ii = vv
+	return nil
+}
 
 // StringSet represents an array expressed as a set.
 // (otherwise than a List which would be the default)
 type StringSet []string
 
 // MarshalDynamoDBAttributeValue implements Marshaler
-func (s StringSet) MarshalDynamoDBAttributeValue(item *dynamodb.AttributeValue) error {
-	if len(s) > 0 && item != nil {
-		item.SS = aws.StringSlice(s)
+func (ss StringSet) MarshalDynamoDBAttributeValue(item *dynamodb.AttributeValue) error {
+	if len(ss) > 0 && item != nil {
+		item.SS = aws.StringSlice(ss)
 	}
 	return nil
 }
 
 // UnmarshalDynamoDBAttributeValue implements Unmarshaler
-func (s *StringSet) UnmarshalDynamoDBAttributeValue(item *dynamodb.AttributeValue) error {
+func (ss *StringSet) UnmarshalDynamoDBAttributeValue(item *dynamodb.AttributeValue) error {
 	if item == nil || item.SS == nil {
 		return nil
 	}
 
-	ss := aws.StringValueSlice(item.SS)
-	*s = ss
+	vv := aws.StringValueSlice(item.SS)
+	*ss = vv
 	return nil
 }
--- set_test.go
+++ set_test.go
@@ -7,6 +7,24 @@ import (
 	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
 )
 
+func TestInt64Set(t *testing.T) {
+	want := Int64Set{1, 2, 3}
+
+	item, err := dynamodbattribute.Marshal(want)
+	if err != nil {
+		t.Fatalf("got %v; want nil", err)
+	}
+
+	var got Int64Set
+	err = dynamodbattribute.Unmarshal(item, &got)
+	if err != nil {
+		t.Fatalf("got %v; want nil", err)
+	}
+	if !reflect.DeepEqual(got, want) {
+		t.Fatalf("got %v; want %v", got, want)
+	}
+}
+
 func TestStringSet(t *testing.T) {
 	want := StringSet{"a", "b", "c"}
 
